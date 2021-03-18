b6b1972247e48c0f6e4b1dfb0c5df3c40ed4df52
HelloID73 <aleksey.sazhnikov@mail.ru>
2021-03-18 20:59:09 +0400

    111

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
 
-$str = 'Слепой заходит в магазин, берет собаку поводыря и начинает раскручивать её над головой
-- Мужчина, что вы делаете?
--Да так, осматриваюсь...';
-
-
-$words = str_word_count($str, 1, 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя');
-$words = array_unique($words);
-
-$count = count($words);
-
-$words_count = [];
-
-foreach ($words as $word) {
-
-    preg_match_all('~\b' . $word . '\b~iu', $str, $words_count[$word]);
+for ($i=1000; $i>-1; $i=$i-7){
+    echo "$i-7=".$i."  " ."\n"; PHP_EOL;
 }
-
-
-foreach ($words_count as $word => $word_count) {
-    $entry_count = count($word_count[0]);
-    echo "{$word}" . PHP_EOL;
-}
-
-echo "Всего слов: {$count}";
