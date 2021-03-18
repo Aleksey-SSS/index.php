928dea70236bbbde28587ef68d86400db7a51280
HelloID73 <aleksey.sazhnikov@mail.ru>
2021-03-18 17:02:57 +0400

    111

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
-$b = 1;
-$a=3;
-$c=$a+$b;
-echo $c;
-echo PHP_EOL;
+
+$str = 'Слепой заходит в магазин, берет собаку–поводыря и начинает раскручивать её над головой.
+— Мужчина, что вы делаете?!!
+— Да так, осматриваюсь...';
+
+
+$words = str_word_count($str, 1, 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя');
+$words = array_unique($words);
+
+$count = count($words);
+
+$words_count = [];
+
+foreach ($words as $word) {
+
+    preg_match_all('~\b' . $word . '\b~iu', $str, $words_count[$word]);
+}
+
+
+foreach ($words_count as $word => $word_count) {
+    $entry_count = count($word_count[0]);
+    echo "{$word}: {$entry_count}" . PHP_EOL;
+}
+
+echo "Всего слов: {$count}";
