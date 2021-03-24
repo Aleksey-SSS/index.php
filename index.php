18583518c2bf6b04768c58499386f727daf3dced
HelloID73 <aleksey.sazhnikov@mail.ru>
2021-03-25 01:42:46 +0400

    first commit

--- index.php
+++ index.php
@@ -8,13 +8,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+
+    <style lang="css">
+        .reg-form{
+            text-align: center;
+            width: 100%;
+        }
+    </style>
+
+
 </head>
 <body>
 <div class="reg-form">
     <form action="./" method="post">
+        <h1>Authorization</h1>
+
+        <span> Login:</span>
         <input type ="text" name="login" /> <br>
+        <span> Password: </span>
         <input type ="text" name="password" /> <br>
+        <span> Name: </span>
         <input type ="text" name="name" /> <br>
+        <span> Surname: </span>
         <input type ="text" name="surname" /> <br>
 
         <button type ="submit">
@@ -42,12 +58,7 @@
         <?php endforeach;?>
     <?php endif; ?>
 </div>
-<style lang="css">
-    .reg-form{
-        text-align: center;
-        width: 100%;
-    }
-</style>
+
 
 </body>
 </html>
